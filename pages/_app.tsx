aeb48f7c93bb03e0dd4db01872bdfb4d6be06f4b
atarof <abdul.tarofdar1990@gmail.com>
2023-02-28 22:23:23 +0000

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import type { NextPage } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import GlobalStyle from "../components/styles/Global.styled";
 import React from "react";
 
@@ -7,8 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-
       <GlobalStyle />
+      <Analytics />
     </>
   );
 }
